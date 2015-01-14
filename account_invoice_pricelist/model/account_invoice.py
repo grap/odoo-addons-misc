443afdef2854bddd98d9e4dabc9f0f95bd986807
Sylvain LE GAL <sylvain.legal@grap.coop>
2015-01-14 13:35:15 +0100

    [ADD] 'account_invoice_pricelist' new module that add pricelist field on account.invoice;

---
+++ account_invoice_pricelist/model/account_invoice.py
@@ -0,0 +1,61 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Account - Price List on Invoice for Odoo
+#    Copyright (C) 2015-Today GRAP (http://www.grap.coop)
+#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import fields, osv
+from openerp.osv.orm import Model
+from openerp.tools.translate import _
+
+class account_invoice(Model):
+    _inherit = 'account.invoice'
+
+    def _get_partner_pricelist_id(
+            self, cr, uid, ids, name, arg, context=None):
+        res = {}
+        for ai in self.browse(cr, uid, ids, context=context):
+            res[ai.id] = False
+            if ai.type in ('out_invoice', 'out_refund'):
+                res[ai.id] = ai.partner_id.property_product_pricelist\
+                    and ai.partner_id.property_product_pricelist.id
+            elif ai.type in ('in_invoice', 'in_refund'):
+                res[ai.id] = ai.partner_id.property_product_pricelist_purchase\
+                    and ai.partner_id.property_product_pricelist_purchase.id
+            else:
+                raise osv.except_osv(
+                    _('Not Implemented!'),
+                    _("""Can not compute Partner Pricelist for this"""
+                    """ type of invoice: '%s'.""" % (ai.type)))
+        return res
+
+    # Columns Section
+    _columns = {
+        'partner_pricelist_id': fields.function(
+            _get_partner_pricelist_id, string='Partner Pricelist',
+            readonly=True, type='many2one', relation='product.pricelist',
+            help="""The pricelist of the partner, when the invoice is"""
+            """ created or the partner has changed. This is a technical"""
+            """ field used to reporting.""",
+            store={'account.invoice': (
+                lambda self, cr, uid, ids, context=None: ids,
+                [
+                    'partner_id',
+                ], 10)}),
+    }
