72ec4bc3a143d4a0011a5a516d15c32ee51c6d43
Sylvain LE GAL <sylvain.legal@grap.coop>
2015-04-08 02:01:38 +0200

    [FIX] dependencies

--- sale_food/__openerp__.py
+++ sale_food/__openerp__.py
@@ -52,7 +52,7 @@ Python librairies required on Debian System:
         'product',
         'l10n_fr_department',
         'report_webkit',
-        'web_color',
+        'web_widget_color',
     ],
     'data': [
         'security/ir_rule.xml',
