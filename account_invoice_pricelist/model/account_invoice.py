9e9791af601cf4f9dd0935ce5c771ec43cc29d66
Sylvain LE GAL <sylvain.legal@grap.coop>
2015-05-26 12:26:16 +0200

    [FIX] 'account_invoice_pricelist' get context value only if uid = 1

--- account_invoice_pricelist/model/account_invoice.py
+++ account_invoice_pricelist/model/account_invoice.py
@@ -20,6 +20,7 @@
 #
 ##############################################################################
 
+from openerp import SUPERUSER_ID
 from openerp.osv import fields, osv
 from openerp.osv.orm import Model
 from openerp.tools.translate import _
@@ -51,7 +52,8 @@ class account_invoice(Model):
             # Normal behaviour
             # Note : In a functional field, SUPERUSER_ID is used in uid
             # So, we will used preferently context('uid') value
-            uid = context.get('uid', False) or uid
+            if uid == SUPERUSER_ID:
+                uid = context.get('uid', False) or uid
 
             for ai in self.browse(cr, uid, ids, context=context):
                 if ai.type in ('out_invoice', 'out_refund'):
