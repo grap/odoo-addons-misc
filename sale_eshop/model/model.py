7565d08d8b346bbc76042a4b45921eeb8e3a25c5
Sylvain LE GAL <sylvain.legal@grap.coop>
2015-11-02 09:18:06 +0100

    [FLAKE8]

--- sale_eshop/model/model.py
+++ sale_eshop/model/model.py
@@ -61,8 +61,8 @@ _ESHOP_OPENERP_MODELS = {
 }
 
 
-
 _eshop_backup_write_function = Model.write
+
 
 def new_write_function(self, cr, uid, ids, vals, context=None):
     res = _eshop_backup_write_function(
@@ -73,35 +73,29 @@ def new_write_function(self, cr, uid, ids, vals, context=None):
         company_obj = self.pool['res.company']
         # It's a model loaded and cached by the eShop
         eshop_fields = eshop_model['fields']
-        
         update_fields = vals.keys()
         intersec_fields = [x for x in eshop_fields if x in update_fields]
-        print "************"
-        print eshop_fields
-        print update_fields
-        print intersec_fields
         if intersec_fields:
             # Some fields are loaded and cached by the eShop
             if eshop_model['type'] == 'single':
                 company_ids = [x for x in [user_obj._get_company(
-                            cr, uid, context=context)]
-                                if x in company_obj.search(cr, SUPERUSER_ID, [('has_eshop', '=', True)], context=context)]
+                    cr, uid, context=context)] if x in company_obj.search(
+                        cr, SUPERUSER_ID,
+                        [('has_eshop', '=', True)],
+                        context=context)]
                 if not company_ids:
                     raise except_osv(_('Error !'), _(
                         "You can not change this values because you have"
                         " not selected the good company."))
-                
             else:
                 company_ids = company_obj.search(
                     cr, SUPERUSER_ID, [('has_eshop', '=', True)],
                     context=context)
-            print company_ids
             for company in company_obj.browse(
                     cr, SUPERUSER_ID, company_ids, context=context):
                 for id in ids:
                     url = company.eshop_invalidation_cache_url\
                         + self._name + '/' + str(id)
-                    print url
                     # TODO IMPROVE ME, auth=('user', 'pass')
                     req = requests.get(url)
                     if req.status_code != 200:
