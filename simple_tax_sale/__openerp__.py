5648449f324e86f18fea50e1a69d1bda502ba8b3
Sylvain LE GAL <sylvain.legal@grap.coop>
2016-06-17 16:00:05 +0200

    [PORT][8.0] 'simple_tax_sale' : full refactoring;

--- simple_tax_sale/__openerp__.py
+++ simple_tax_sale/__openerp__.py
@@ -1,47 +1,13 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    Sale - Simple Tax module for Odoo
-#    Copyright (C) 2015-Today GRAP (http://www.grap.coop)
-#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# Copyright (C) 2015-Today GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Sale - Simple Tax',
     'summary': 'Easy Switch between VAT Excluded and VAT Included For Sale',
-    'version': '0.1',
+    'version': '8.0.1.0.0',
     'category': 'Sale',
-    'description': """
-Easy Switch between VAT Excluded and VAT Included For Sale Module
-=================================================================
-
-Feature:
---------
-* Possibility to switch between Price VAT Included and Price VAT Excluded
-  when editing a Sale Order;
-
-See simple_tax_account for more information
-
-Copyright, Authors and Licence:
--------------------------------
-    * Copyright: 2015, GRAP: Groupement Régional Alimentaire de Proximité;
-    * Author:
-        * Sylvain LE GAL (https://twitter.com/legalsylvain);
-    * Licence: AGPL-3 (http://www.gnu.org/licenses/);""",
     'author': 'GRAP',
     'website': 'http://www.grap.coop',
     'license': 'AGPL-3',
@@ -50,8 +16,8 @@ Copyright, Authors and Licence:
         'sale',
     ],
     'data': [
-        'view/view.xml',
+        'views/view_sale_order.xml',
     ],
     'auto_install': True,
-    'installable': False,
+    'installable': True,
 }
