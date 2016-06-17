f1e1862f5899b9222d2be5d974c213b9285eb014
Sylvain LE GAL <sylvain.legal@grap.coop>
2016-06-17 13:27:29 +0200

    [PORT][8.0] 'simple_tax_account' full refactoring / OCA convention / description ...;

--- simple_tax_account/__openerp__.py
+++ simple_tax_account/__openerp__.py
@@ -1,69 +1,13 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    Account - Simple Tax module for Odoo
-#    Copyright (C) 2015-Today GRAP (http://www.grap.coop)
-#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# Copyright (C) 2015-Today GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Account - Simple Tax',
     'summary': 'Easy Switch between VAT Excluded and VAT Included',
-    'version': '0.1',
+    'version': '8.0.1.0.0',
     'category': 'Account',
-    'description': """
-Easy Switch between VAT Excluded and VAT Included
-=================================================
-
-Use case:
----------
-This module is useful in the following case:
-* Odoo is implemented in a country with simple Taxes system, (Like in France)
-  with generaly only one Tax type='percent'. (VAT)
-* Users define allways product price with VAT included. (or allways excluded);
-* Some customers / suppliers wants quotation / invoices with the other system;
-
-Feature:
---------
-* On account.tax, add a new field 'simple_tax_id' that is the according VAT
-  with or without TAX included. Sample:
-      * TAX A: VAT 10% included;
-      * TAX B : VAT 10% excluded;
-      * TAX A and TAX B will be linked together;
-* On res.partner, add a new field selection 'simple_tax_type' with
-  the following values:
-    * 'none' : (default) undefined, the Tax will be the tax of the product;
-    * 'excluded': All price will be recomputed with Tax excluded;
-    * 'included': All price will be recomputed with Tax inluded;
-* Possibility to switch between Price VAT Included and Price VAT Excluded
-  when editing a Account Invoice;
-
-Related Modules:
-----------------
-* This following modules provide the same behouviour:
-    * 'simple_tax_sale' for sale module;
-    * 'simple_tax_purchase' for purchase module;
-
-Copyright, Authors and Licence:
--------------------------------
-    * Copyright: 2015, GRAP: Groupement Régional Alimentaire de Proximité;
-    * Author:
-        * Sylvain LE GAL (https://twitter.com/legalsylvain);
-    * Licence: AGPL-3 (http://www.gnu.org/licenses/);""",
     'author': 'GRAP',
     'website': 'http://www.grap.coop',
     'license': 'AGPL-3',
@@ -71,7 +15,9 @@ Copyright, Authors and Licence:
         'account',
     ],
     'data': [
-        'view/view.xml',
+        'views/view_res_partner.xml',
+        'views/view_account_invoice.xml',
+        'views/view_account_tax.xml',
     ],
     'demo': [
         'demo/res_groups.yml',
@@ -79,5 +25,5 @@ Copyright, Authors and Licence:
         'demo/res_partner.yml',
         'demo/product_product.yml',
     ],
-    'installable': False,
+    'installable': True,
 }
