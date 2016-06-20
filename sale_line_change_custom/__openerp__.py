77bba1cd0a7f20cd15fded78bcfaa0b0c7a95170
Sylvain LE GAL <sylvain.legal@grap.coop>
2016-06-20 20:57:24 +0200

    [PORT][8.0] 'sale_line_change_custom' full refactoring;

--- sale_line_change_custom/__openerp__.py
+++ sale_line_change_custom/__openerp__.py
@@ -1,46 +1,13 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    Sale - Custom Behaviour on Line Changes module for Odoo
-#    Copyright (C) 2015-Today Noemis (http://www.noemis.fr)
-#    Copyright (C) 2015-Today GRAP (http://www.grap.coop)
-#
-#    @author: Sylvain LE GAL (https://www.twitter.com/legalsylvain)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# Copyright (C) 2015-Today GRAP (http://www.grap.coop)
+# Copyright (C) 2015-Today Noemis (http://www.noemis.fr)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Sale - Custom Behaviour on Line Changes',
-    'version': '1.1',
+    'version': '8.0.1.0.0',
     'category': 'Sale',
-    'description': """
-Customization when Sale Order Lines Change during quotation
-===========================================================
-
-Let users customize quotation options:
-
-* Possibility to disable stock warning;
-
-Note
-----
-
-This module is not active by default, and settings must be done by users,
-because there are managed by groups.
-
-    """,
     'author': 'GRAP,Noemis',
     'license': 'AGPL-3',
     'depends': [
@@ -49,5 +16,5 @@ because there are managed by groups.
     'data': [
         'security/res_groups.yml',
     ],
-    'installable': False,
+    'installable': True,
 }
--- sale_line_change_custom/models/sale_order_line.py
+++ sale_line_change_custom/models/sale_order_line.py
@@ -1,27 +1,29 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#    See __openerp__.py file for copyright and licences
-##############################################################################
+# -*- coding: utf-8 -*-
+# Copyright (C) 2015-Today GRAP (http://www.grap.coop)
+# Copyright (C) 2015-Today Noemis (http://www.noemis.fr)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp.osv.orm import Model
+from openerp import models, api
 
 
-class sale_order_line(Model):
+class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    def product_id_change(
-            self, cr, uid, ids, pricelist, product, qty=0, uom=False,
+    @api.multi
+    def product_id_change_with_wh(
+            self, pricelist, product, qty=0, uom=False,
             qty_uos=0, uos=False, name='', partner_id=False, lang=False,
             update_tax=True, date_order=False, packaging=False,
-            fiscal_position=False, flag=False, context=None):
-        res = super(sale_order_line, self).product_id_change(
-            cr, uid, ids, pricelist, product, qty=qty, uom=uom,
-            qty_uos=qty_uos, uos=uos, name=name, partner_id=partner_id,
-            lang=lang, update_tax=update_tax, date_order=date_order,
-            packaging=packaging, fiscal_position=fiscal_position, flag=flag,
-            context=context)
-        if self.pool['res.users'].has_group(
-                cr, uid,
+            fiscal_position=False, flag=False, warehouse_id=False,
+            context=None):
+        res = super(SaleOrderLine, self).product_id_change_with_wh(
+            pricelist, product, qty=qty, uom=uom, qty_uos=qty_uos, uos=uos,
+            name=name, partner_id=partner_id, lang=lang, update_tax=update_tax,
+            date_order=date_order, packaging=packaging,
+            fiscal_position=fiscal_position, flag=flag,
+            warehouse_id=warehouse_id)
+        if self.env.user.has_group(
                 'sale_line_change_custom.group_sale_order_line_no_warning'):
             res.pop('warning', False)
         return res
