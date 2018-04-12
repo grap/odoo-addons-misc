861a2390e90c23c4ae43e00ae8db766559a1fe7a
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-04-12 17:26:49 +0200

    [REF] remove 3 useless modules stock_picking_quick_invoice, pos_order_log, barcodes_generate

--- stock_picking_quick_invoice/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import account_journal
-from . import stock_picking_out
-from . import stock_picking_quick_invoice_line_wizard
-from . import stock_picking_quick_invoice_wizard
