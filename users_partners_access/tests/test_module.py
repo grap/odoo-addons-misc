62c4f826ad15ea5617c04d0e9ab1f2046cacfc11
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-10-22 14:51:06 +0200

    [REF] users_partners_access

--- users_partners_access/tests/test_module.py
+++ users_partners_access/tests/test_module.py
@@ -4,93 +4,35 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp.tests.common import TransactionCase
+from openerp.exceptions import Warning as UserError
 
 
 class TestModule(TransactionCase):
 
     def setUp(self):
         super(TestModule, self).setUp()
+        self.ResUsers = self.env['res.users']
+        self.demo_user = self.env.ref('base.user_demo')
+        self.demo_partner = self.env.ref('base.partner_demo')
 
+    def test_01_create_user(self):
+        user = self.ResUsers.create({
+            'name': 'Users Partners Access Test',
+            'login': 'login@users_partners_access.com',
+        })
+        self.assertEqual(
+            user.partner_id.active, False,
+            "User's partner should be disabled when created")
 
-    #     self.partner_obj = self.env['res.partner']
-    #     self.separator = '*'
-    #     self.setting_obj = self.env['base.config.settings']
-    #     self.name_with_separator = 'Test%sAbc' % self.separator
-    #     self.name_without_separator = 'TestAbc'
+    def test_02_check_access_right(self):
 
-    # # Test Section
-    # def test_01_create_write_partner_enabled(self):
-    #     self._enable_settings(True)
-    #     # Create Partner
-    #     vals = {
-    #         'name': self.name_with_separator,
-    #     }
-    #     partner = self.partner_obj.create(vals)
-    #     self.assertEqual(
-    #         partner.name, self.name_without_separator,
-    #         "Special char in the field should be removed during creation")
+        # Without Correct access right, should fail
+        with self.assertRaises(UserError):
+            self.demo_partner.sudo(self.demo_user).write({
+                'name': 'Test',
+            })
 
-    #     partner.write(vals)
-    #     self.assertEqual(
-    #         partner.name, self.name_without_separator,
-    #         "Special char in the field should be removed during update")
-
-    # def test_02_create_write_partner_disbled(self):
-    #     self._enable_settings(False)
-    #     # Create Partner
-    #     vals = {
-    #         'name': self.name_with_separator,
-    #     }
-    #     partner = self.partner_obj.create(vals)
-    #     self.assertEqual(
-    #         partner.name, self.name_with_separator,
-    #         "Special char in the field shouldn't be removed during creation")
-
-    #     partner.write(vals)
-    #     self.assertEqual(
-    #         partner.name, self.name_with_separator,
-    #         "Special char in the field shouldn't be removed during update")
-
-    # def test_03_search_partner(self):
-    #     ordered_domain =\
-    #         [('display_name', 'ilike', 'Abc%sDef' % self.separator)]
-    #     unordered_domain =\
-    #         [('display_name', 'ilike', 'Def%sAbc' % self.separator)]
-
-    #     # Initial Search
-    #     self._enable_settings(False)
-    #     initial_search = len(self.partner_obj.search(ordered_domain))
-
-    #     # Create Partner
-    #     vals = {
-    #         'name': 'Abc other Word Def',
-    #     }
-    #     self.partner_obj.create(vals)
-
-    #     # First Search (Feature disabled)
-    #     disabled_feature_search = len(self.partner_obj.search(ordered_domain))
-    #     self.assertEqual(
-    #         initial_search, disabled_feature_search,
-    #         "Search function should not return items if settins is disabled")
-
-    #     # Second search (ordered) (Feature enabled)
-    #     self._enable_settings(True)
-    #     ensabled_feature_search_ordered = len(
-    #         self.partner_obj.search(ordered_domain))
-    #     self.assertEqual(
-    #         ensabled_feature_search_ordered, initial_search + 1,
-    #         "Multi search in ordered mode failed.")
-
-    #     ensabled_feature_search_unordered = len(
-    #         self.partner_obj.search(unordered_domain))
-    #     self.assertEqual(
-    #         ensabled_feature_search_unordered, initial_search + 1,
-    #         "Multi search in unordered mode failed.")
-
-    # def _enable_settings(self, enable):
-    #     setting = self.setting_obj.create({})
-    #     if enable:
-    #         setting.multi_search_partner_separator = self.separator
-    #     else:
-    #         setting.multi_search_partner_separator = False
-    #     setting.set_multi_search_partner_separator()
+        # With Correct access right, should success
+        self.demo_partner.write({
+            'name': 'Test',
+        })
