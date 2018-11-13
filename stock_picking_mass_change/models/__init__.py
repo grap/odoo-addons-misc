9c7edcf04a88bf4930285b35673c389f1628c07e
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-11-13 12:45:22 +0100

    [REF] move stock_picking_quick_edit and stock_picking_mass_change into grap-odoo-business

--- stock_picking_mass_change/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# coding: utf-8
-from . import stock_picking_mass_change_wizard
-from . import stock_picking_mass_change_wizard_line
--- stock_picking_quick_edit/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# coding: utf-8
-from . import stock_picking_quick_edit_wizard
-from . import stock_picking_quick_edit_wizard_current_move
-from . import stock_picking_quick_edit_wizard_new_move
