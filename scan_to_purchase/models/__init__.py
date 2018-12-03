040070170c3f61b5e2f925037696b30343f33bfa
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-12-03 10:34:41 +0100

    [REF] move scan_to_purchase into bin

--- scan_to_purchase/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import res_users
-from . import purchase_order
-from . import product_product
-from . import res_company
--- scan_to_purchase/static/www/controllers/c_credit.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-
-angular.module('scan_to_purchase').controller(
-        'CreditCtrl', [
-        '$scope','$rootScope', 'jsonRpc', '$state', '$translate',
-        function ($scope, $rootScope, jsonRpc, $state, $translate) {
-
-}]);
